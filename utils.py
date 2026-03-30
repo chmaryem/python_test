aef9affa8a1bbe79dcf132411866e76f554dbc56
chmaryem <mariemchalghoumi6666@gmail.com>
2026-03-30 12:07:54 +0100

    first commit4

--- utils.py
+++ utils.py
@@ -2,8 +2,9 @@
 
 def execute_query(connection, user_input):
     try:
-       
-        return connection.execute(query, (user_input,))
+       query = "SELECT * FROM your_table WHERE your_column = ?"
+       with connection.execute(query, (user_input,)) as cursor:
+              return cursor.fetchall()
     except Exception as e:
         print(f"An error occurred: {e}")
         return None
