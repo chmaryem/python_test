3032b4322f1dca6bfba3d1959fb6a66dc482c1fb
chmaryem <mariemchalghoumi6666@gmail.com>
2026-04-22 11:05:14 +0100

    first commit

--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@
 
 def execute_query(connection, user_input):
     try:
+        query = "SELECT * FROM your_table WHERE your_column = %s"  # Replace with your actual query
        
         return connection.execute(query, (user_input,))
     except Exception as e:
